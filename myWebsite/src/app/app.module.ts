3bf3ef28d8fdc75638cc05177421852406f5230a
Apurva <apurva.kumar.srivastav@gmail.com>
2018-09-29 13:14:26 +0530

    implemented gridster

---
+++ myWebsite/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { GridsterModule } from 'angular-gridster2';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    GridsterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
